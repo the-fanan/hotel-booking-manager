e4ae9fd5011df3896e7723fb9815a5ffafbebbc1
Fanan Dala <fanan.dala@yahoo.com>
2019-07-02 19:39:50 +0100

    chore: added csrf token meta to index page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,12 +3,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>Codeline Hotel Manager</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-			<div id="admin-app"></div>
-			<script src="{{ url(mix('/js/admin/app.js')) }}" type="text/javascript"></script>
+        <div id="admin-app"></div>
+        <script src="{{ url(mix('/js/admin/app.js')) }}" type="text/javascript"></script>
     </body>
 </html>
