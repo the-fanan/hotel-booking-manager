bfa3622b45b19026fe8be7606497136bc11d1ac9
Fanan Dala <fanan.dala@yahoo.com>
2019-07-03 00:38:59 +0400

    feat(backend): added login

---
+++ app/Http/Controllers/API/Admin/Auth/LoginController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\API\Admin\Auth;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use Auth;
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Get the guard to be used during authentication.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return Auth::guard('admin');
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string'],
+        ]);
+    }
+
+    /**
+     * Handle a Login request for the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function login(Request $request)
+    {
+        $validator = $this->validator($request->all());
+        if ($validator->fails()) {
+            $error["message"] = "Invalid or missing input fields";
+            $validation_messages = "";
+            foreach ($validator->errors()->toArray() as $name => $value) {
+                $validation_messages .= $value[0] . " ";
+            }
+            $error["validation_messages"] = $validation_messages;
+            $error["status"] = "error";
+            return response()->json($error, 401);
+        }
+
+        if (!$this->guard()->attempt($request->only(['email', 'password']))) {
+            //if user authentication fails
+            $error["message"] = "Invalid credentials";
+            $error["status"] = "error";
+            return response()->json($error, 401);
+        }
+
+        $user = Admin::where('email', '=', $request->email)->first();
+        $APItokenDetails = $user->createToken('shecluded');//this decouplin is done incase other details are needed to be retrieved later
+        $APItoken = $APItokenDetails->accessToken;
+        //employment details
+        $user->employment_details = $user->getEmploymentDetails();
+        //response
+        $response = [
+            "message" => "Login Successful!",
+            "user" => $user,
+            "status" => "success",
+            "token" => $APItoken
+        ];
+
+        return response()->json($response, 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(["prefix" => "admin", "namespace" => "API\Admin"], function() {
+    Route::group(["prefix" => "auth", "namespace" => "Auth"], function() {
+        Route::post("/login", "LoginController@login");
+    });
+});
